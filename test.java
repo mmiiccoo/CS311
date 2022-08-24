e0076d2b0ed85114263d8e1ad6807b1c2a757a27
mmiiccoo <111960349+mmiiccoo@users.noreply.github.com>
2022-08-24 15:23:55 +0800

    Update test.java

--- test.java
+++ test.java
@@ -5,5 +5,31 @@ public class test {
     public static void main(String[] args) {
 
     }
+    
+    public static String whatCentury(int year) {
+      String whatCentury;
+      String w;
+      int lastDigit;
+      int century;
+      
+      
+      century = (year + 99) / 100;
+      
+      lastDigit = century % 10 ;
+      
+      if (lastDigit == 1){
+        w = "st";
+      } else if (lastDigit == 2){
+        w = "nd";
+      } else if (lastDigit == 3){
+        w = "rd";
+      } else {
+        w = "th";
+      }
+      
+      whatCentury = century + w;
+      // coding here
+      return whatCentury;
+    }
 
 }
